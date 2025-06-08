d59ea8ed2cf184028373a99a2574ca50f0e831f7
rhm <rhm@andpianoforall.com>
2025-06-08 17:23:50 +0200

    db.php contraseña actualizada

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
     $host = '10.0.2.16';
     $usuario = 'rhm';
-    $password = '*9A37A5B922A005DB44AD71C89E2CFBEBEE04950';
+    $password = 'ckl_*opt21A';
     $base_datos = 'piano';
 
     $conn = new mysqli($host, $usuario, $password, $base_datos);
