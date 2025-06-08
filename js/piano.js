cb0924c48091038b71dc2b648c05f083fe1ccf33
rhm <rhm@andpianoforall.com>
2025-06-08 21:21:35 +0200

    notas actualizadas para 2 octavas

--- js/piano.js
+++ js/piano.js
@@ -1,17 +1,28 @@
 const keyMap = {
-  'a': 'C6',
-  's': 'D6',
-  'd': 'E6',
-  'f': 'F6',
-  'g': 'G6',
-  'h': 'A6',
-  'j': 'B6',
+  'a': 'c4',
+  'w': 'c-4',
+  's': 'd4',
+  'e': 'd-4',
+  'd': 'e4',
+  'f': 'f4',
+  't': 'f-4',
+  'g': 'g4',
+  'y': 'g-4',
+  'h': 'a4',
+  'u': 'a-4',
+  'j': 'b4',
+
+  'k': 'c5',
+  'o': 'c-5',
+  'l': 'd5',
+  'p': 'd-5',
+  'ñ': 'e5',
 };
 
 function playSound(note) {
-  const audio = new Audio(`/sounds/${note.toLowerCase()}.mp3`);
-  audio.currentTime = 0; // reiniciar si se pulsa rápido
-  audio.play().catch(err => console.warn('No se pudo reproducir el sonido:', err));
+  const audio = new Audio(`/sounds/${note}.mp3`);
+  audio.currentTime = 0;
+  audio.play().catch(err => console.warn('No se pudo reproducir:', err));
 }
 
 document.addEventListener('keydown', (event) => {
