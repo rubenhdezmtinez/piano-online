01530d983b1b0ddd6597645a2f382c2350dc6181
rhm <rhm@andpianoforall.com>
2025-06-08 20:40:43 +0200

    piano notas actualizadas

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
 </main>
-<footer>
+<footer class="footer">
     <p>&copy; 2025 Piano Online</p>
 </footer>
 </body>
--- js/piano.js
+++ js/piano.js
@@ -1,21 +1,15 @@
 const keyMap = {
-  'a': 'C6',
-  'w': 'C#6',
-  's': 'D6',
-  'e': 'D#6',
-  'd': 'E6',
-  'f': 'F6',
-  't': 'F#6',
-  'g': 'G6',
-  'y': 'G#6',
-  'h': 'A6',
-  'u': 'A#6',
-  'j': 'B6',
-  'k': 'C7'
+    'a': 'C6',
+    's': 'D6',
+    'd': 'E6',
+    'f': 'F6',
+    'g': 'G6',
+    'h': 'A6',
+    'j': 'B6',
 };
 
 function playSound(note) {
-  const audio = new Audio(`/sounds/${note}.mp3`);
+  const audio = new Audio('/sounds/${note}.mp3');
   audio.play();
 }
 
