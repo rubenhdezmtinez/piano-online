d1e1e2eb72ee02a98eaba5c8be08071ea8e10b5b
rhm <rhm@andpianoforall.com>
2025-06-08 20:02:05 +0200

    piano añadido

--- templates/home.php
+++ templates/home.php
@@ -1,2 +1,20 @@
 <h2>Bienvenido a la página principal</h2>
 <p>Este es el contenido de inicio.</p>
+<div class="piano">
+    <div class="white-keys">
+        <div class="white"></div>
+        <div class="white"></div>
+        <div class="white"></div>
+        <div class="white"></div>
+        <div class="white"></div>
+        <div class="white"></div>
+        <div class="white"></div>
+    </div>
+    <div class="black-keys">
+        <div class="black" style="left: 30px;"></div>
+        <div class="black" style="left: 70px;"></div>
+        <div class="black" style="left: 150px;"></div>
+        <div class="black" style="left: 190px;"></div>
+        <div class="black" style="left: 230px;"></div>
+    </div>
+</div>
