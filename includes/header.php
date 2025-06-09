98fffd1206a40481c236756f194e0c400b501a8d
rhm <rhm@andpianoforall.com>
2025-06-09 17:21:03 +0200

    frontend v1.1

--- includes/header.php
+++ includes/header.php
@@ -1,25 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Piano online</title>
-    <link rel="stylesheet" href="css/estilos.css">
-    <script src="/js/piano.js"></script>
-</head>
-<body>
-    <header>
-        <h1>Piano online</h1>
-        <nav>
-            <a href="index.php">Piano</a>
-            <a href="login.php">Iniciar sesión</a>
-            <a href="registro.php">Registrarse</a>
-        </nav>
-    </header>
-    <div class="main-container">
-        <?php include 'includes/header.php'; ?>
-        <?php include 'includes/piano.php' ?>
-    </div>
-
-    <?php include 'includes/footer.php'; ?>
-<main>
+<div class="header">
+    <h2>Piano online</h2>
+    <a href="index.php">Piano</a><br>
+    <a href="login.php">Iniciar sesión</a><br>
+    <a href="registro.php">Registrarse</a>
+</div>
