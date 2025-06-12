e90bc44737791bceafd92dca8278f6f848e84d98
rhm <rhm@andpianoforall.com>
2025-06-12 07:53:27 +0200

    ultima revisión v1.1

--- login.php
+++ login.php
@@ -1,9 +1,13 @@
+
 <?php
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require_once 'db.php';
+
+echo "Hasta aquí todo bien<br>";
+
 $mensaje = '';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
